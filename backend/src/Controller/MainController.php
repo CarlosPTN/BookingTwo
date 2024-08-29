68f22a2c76185b4e0c7a198c4e2a9b462c4ae64b
CarlosPTN <carlosmioli1@gmail.com>
2024-08-29 16:06:42 +0200

    Added twig for a pseudo homepage

--- backend/src/Controller/MainController.php
+++ backend/src/Controller/MainController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // everytime we have a php atribute, we need the corresponding use statement for it in the same file
 // every page in symfony is a route controller
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Hotel</strong>: your home comfort in Leiria!');
+        $starshipCount = 457;
+        $myShip = [
+            'name'=> 'USS XXXXXXX',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', ['numberOfStarships' => $starshipCount, 'myShip'=>$myShip]);
+   
     }
 }
